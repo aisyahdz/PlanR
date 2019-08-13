2328853772bfea680ed695687a990ddaf02eb9be
AisyahDzulqaidah <aisyahdzulqaidah@yahoo.co.id>
2019-08-13 07:55:13 +0700

    Add Contexts : Reminder and Task

---
+++ PlanR/Contexts/Reminder/TaskReminder.swift
@@ -0,0 +1,9 @@
+//
+//  TaskReminder.swift
+//  PlanR
+//
+//  Created by Aisyah Dzulqaidah on 30/07/19.
+//  Copyright © 2019 Aisyah Dzulqaidah. All rights reserved.
+//
+
+import Foundation
---
+++ PlanR/Contexts/Task/TaskListManagerTest.swift
@@ -0,0 +1,22 @@
+//
+//  TaskListManagerTest.swift
+//  PlanRTests
+//
+//  Created by Aisyah Dzulqaidah on 09/08/19.
+//  Copyright © 2019 Aisyah Dzulqaidah. All rights reserved.
+//
+
+import XCTest
+
+class TaskListManagerTest: XCTestCase {
+
+    func testAddingNewTaskWillAddNewItemInTaskList(){
+    
+    }
+    
+    func testDeleteTaskWillDeleteItemInTAskList(){
+        
+    }
+    
+
+}
