87ac33ac732ec4a4b3b649a7656853b17a2e2f2c
AisyahDzulqaidah <aisyahdzulqaidah@yahoo.co.id>
2019-08-20 08:41:24 +0700

    Add TaskListManager

--- PlanR/Contexts/Task/TaskListManager.swift
+++ PlanR/Contexts/Task/TaskListManager.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+var task = [String]()
+struct TaskListManager {
+    func add(_ taskItem: String){
+        task.append(taskItem)
+    }
+    
+    func update(index: Int, taskItem: String){
+        task[index] = taskItem
+    }
+    
+    func getItem(index: Int) -> String {
+        return task[index]
+    }
+    
+    func deleteItem(index: Int) {
+        task.remove(at: index)
+    }
+    
+    func taskCount() -> Int {
+        return task.count
+    }
+    
+}
+
+func deleteAllTask(){
+    task.removeAll()
+}
--- PlanR/Contexts/Task/TaskListManagerTest.swift
+++ PlanR/Contexts/Task/TaskListManagerTest.swift
@@ -7,14 +7,48 @@
 //
 
 import XCTest
+@testable import PlanR
 
 class TaskListManagerTest: XCTestCase {
-
-    func testAddingNewTaskWillAddNewItemInTaskList(){
     
+    override func setUp() {
+        deleteAllTask()
+    }
+
+    func testAddingNewTaskInEmptyTaskListWillAddNewItemInFirstIndex(){
+        let taskList = TaskListManager()
+        let task = "Makan"
+        taskList.add(task)
+        XCTAssertEqual(taskList.getItem(index: 0), "Makan")
     }
     
-    func testDeleteTaskWillDeleteItemInTAskList(){
+    func testUpdateTaskInSpecificIndexWillUpdateTheTaskInThatIndex(){
+        let taskList = TaskListManager()
+        let task = "Makan"
+        taskList.add(task)
+        XCTAssertEqual(taskList.getItem(index: 0), "Makan")
+        taskList.update(index: 0,  taskItem: "Mandi")
+        XCTAssertEqual(taskList.getItem(index: 0), "Mandi")
+    }
+
+    func testDeleteTaskWillDeleteItemInTaskList(){
+        let taskList = TaskListManager()
+        let task = "Makan"
+        taskList.add(task)
+        XCTAssertEqual(taskList.getItem(index: 0), "Makan")
+        taskList.add("Mandi")
+        XCTAssertEqual(taskList.getItem(index: 1), "Mandi")
+        taskList.deleteItem(index: 1)
+        XCTAssertEqual(taskList.taskCount(), 1)
+        
+    }
+    
+    func testDeleteAllTask() {
+        let taskList = TaskListManager()
+        taskList.add("Mandi")
+        XCTAssertEqual(task[0], "Mandi")
+        deleteAllTask()
+        XCTAssertTrue(task.isEmpty)
         
     }
     
